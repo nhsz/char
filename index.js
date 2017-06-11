070bfa3b0f5eb0ab07287dd4878e9973936703b2
Nicolás Quiroz <nh.quiroz@gmail.com>
2017-06-11 11:43:38 -0300

    Update package.json

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict'
 
-module.exports = (code) => {
+module.exports = code => {
   return String.fromCodePoint(code)
 }
--- test.js
+++ test.js
@@ -1,23 +1,14 @@
 import test from 'ava'
-import chr from '../lib'
+import chr from './'
 
-test('Test 1', t => {
+test(t => {
   t.is(chr(65), 'A')
-})
-
-test('Test 2', t => {
   t.is(chr(97), 'a')
-})
-
-test('Test 3', t => {
   t.is(chr(8364), '€')
-})
-
-test('Test 4', t => {
   t.is(chr(64), '@')
 })
 
-test('Test 5: out of valid range', t => {
+test(t => {
   const error = t.throws(() => {
     chr(-1)
   }, RangeError)
